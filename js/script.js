9e305f2673a8b9464ba376fcf90c3486e4c3cbc9
Madrigal11 <kurupgokul11@gmail.com>
2019-04-23 19:06:08 +0530

    ctrl s functionality

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = function() {
     var editor = document.getElementById("mainarea");
     var save = document.getElementById("save");
      log.open()
-     save.addEventListener("click",function() {
+    /*  save.addEventListener("click",function() {
               var text = editor.value;
               if (text.replace(/ /g,'') != '') {
                 // Create the todo item.
@@ -12,9 +12,19 @@ window.onload = function() {
                     console.log(entry);
                 });
               }
-     });
+     }); */
+     Mousetrap.bind('ctrl+s', function(e) {
+        e.preventDefault();
+        var text = editor.value;
+        if (text.replace(/ /g,'') != '') {
+          // Create the todo item.
+         log.createEntry(text, function(entry) {
+              console.log(entry);
+          });
+        }
+    });
      editor.addEventListener("keydown",function(e) {
-        var event = e.which;
+        var event = e.which || e.keyCode;
         switch(event){
             case 9: e.preventDefault();
                     var s = this.selectionStart;
