a0f4173661f8b667d748cfd532ca9c9c0b729d0a
Madrigal11 <kurupgokul11@gmail.com>
2019-04-24 16:40:58 +0530

    electron saving fixes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var plannerType = 1;
+ /*   econtainer.style.borderLeft = "0.3rem #2196f3 solid"; */
+var plannerType = 0;
 function fetch() {
   log.fetchEntries(plannerType,function(entries) {
       var List = document.getElementById("entries");
@@ -6,15 +7,36 @@ function fetch() {
       for(var i=0;i<entries.length;i++) {
         var entry = entries[(entries.length - 1 - i)];
         var econtainer = document.createElement("div");
-        econtainer.id = 'entry-' + entry.timestamp;
+        econtainer.id = entry.timestamp;
         econtainer.className ="logentries";
-        var span = document.createElement('span');
-        span.innerHTML =  entry.text.split('\n')[0];
-        econtainer.appendChild(span);
+        var title = document.createElement('span');
+        var date = document.createElement('span');
+        title.className = "econtainer_Title";
+        title.innerHTML =  entry.text.split('\n')[0] +"<br>" ;
+        date.classList = "econtainer_Date";
+        date.innerHTML = "- " + entry.timestamp;
+        econtainer.appendChild(title);
+        econtainer.appendChild(date);
         List.appendChild(econtainer);      
 
       } 
  });
+}
+
+function changeMode(p) {
+  var tabs = document.getElementsByClassName("tabs_nav_button");
+  switch(p) {
+    case 3: tabs[3].style.backgroundColor = "#1a237e";
+             break;
+    case 2: tabs[2].style.backgroundColor = "#1a237e";
+            break;
+    case 1: tabs[1].style.backgroundColor = "#1a237e";
+            break;
+    case 0: tabs[0].style.backgroundColor = "#1a237e";
+            break;
+    default:console.log("test");
+  }
+  
 }
 
 window.onload = function() {
@@ -43,6 +65,19 @@ window.onload = function() {
           });
         }
     });
+  
+    save.addEventListener("click",function(){
+      var text = editor.value;
+      text.replace(/\r?\n/g, '<br />');
+      if (text.replace(/ /g,'') != '') {
+        // Create the todo item.
+       log.createEntry(text,plannerType,function(entry) {
+            console.log(entry);
+            M.toast({html: 'Saved !', classes: "toastSave"})
+            fetch()
+        });
+      }
+    });
      editor.addEventListener("keydown",function(e) {
         var event = e.which || e.keyCode;
         switch(event){
@@ -54,7 +89,23 @@ window.onload = function() {
         } 
     
      });
-     
+     changeMode(plannerType);
+     var mode = document.getElementsByClassName("tabs_nav");
+     for(var m=0;m<mode.length;m++) {
+         mode[m].addEventListener("click",function(evt) {
+              for (let k = 0; k < mode.length; k++) {
+                    mode[k].children[1].style.backgroundColor = "grey";  
+              }
+             for (let k = 0; k < mode.length; k++) {
+                   if(mode[k] == evt.target)   {
+                       plannerType = k;
+                       break;
+                   }
+             }
+            changeMode(plannerType);
+         });
+     }
+    
      
 }
  
--- main.js
+++ main.js
@@ -8,9 +8,10 @@ const {app,BrowserWindow,Menu,globalShortcut} = electron;
 
 let mainWindow ;
 
+
 app.on("ready",function() {
      mainWindow = new BrowserWindow({width: 100000, height: 10000})
-     mainWindow.setMenuBarVisibility(false)
+    // mainWindow.setMenuBarVisibility(false)
       
       mainWindow.loadURL(url.format({
            pathname : path.join(__dirname,"index.html"),
@@ -19,7 +20,11 @@ app.on("ready",function() {
       }));
 
       globalShortcut.register('CommandOrControl+S', () => {
-        console.log('CommandOrControl+X is pressed')
+        mainWindow.webContents.on('did-finish-load', ()=>{
+            let code = 'var editor = document.getElementById("mainarea");var text = editor.value;text.replace(/\r?\n/g, "<br />");if (text.replace(/ /g,"") != "") {log.createEntry(text,plannerType,function(entry) {console.log(entry);M.toast({html: "Saved !", classes: "toastSave"})fetch()}); };';
+            mainWindow.webContents.executeJavaScript(code);
+            console.log("test");
+        });
       })
 
      /* const mainMenu = Menu.buildFromTemplate(templatemenu);
